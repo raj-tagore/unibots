1493f5d400c7903e30904f38f1c66427e88ce8cb
Raj Tagore <rajtagore780@gmail.com>
2024-03-07 16:52:01 +0000

    First commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rajtagore/unibotsuk24_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rajtagore/unibotsuk24_ws/devel;/opt/ros/noetic'.split(';') if '/home/rajtagore/unibotsuk24_ws/devel;/opt/ros/noetic' != '' else []
---
+++ src/pilot/src/get_img_subscriber.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class ImageSubscriber:
+    def __init__(self):
+        rospy.init_node('image_subscriber', anonymous=True)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.callback)
+
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except Exception as e:
+            rospy.logerr(e)
+            return
+
+        cv2.imshow("Image", cv_image)
+        cv2.waitKey(1)
+
+def main():
+    image_subscriber = ImageSubscriber()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
